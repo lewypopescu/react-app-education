6e0b4d598f790f61473908f5690f6ee26e411e29
lewypopescu <popesculewy@gmail.com>
2024-07-22 16:48:34 +0200

    functional with hooks

--- src/components/University/University.jsx
+++ src/components/University/University.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import Paper from "../Paper";
 import { HiPencilAlt, HiTrash } from "react-icons/hi";
 
